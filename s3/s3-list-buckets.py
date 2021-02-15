71354a942a65ce07cdb131fc324a506476dfdb73
alykes <anthony.gouskos@gmail.com>
2021-02-15 22:35:04 +1100

    Added an additional function

--- s3/s3-list-buckets.py
+++ s3/s3-list-buckets.py
@@ -1,7 +1,5 @@
 import boto3
 
-s3 = boto3.resource('s3')
-
 def s3_list():
     bucket_list = []
     for bucket in s3.buckets.all():
@@ -10,19 +8,29 @@ def s3_list():
     return(bucket_list)
 
 def s3_bucket_size(bucket):
-    sizeB=0
-    my_bucket=s3.Bucket(bucket)
+    sizeB = 0
+    my_bucket = s3.Bucket(bucket)
     for object in my_bucket.objects.all():
         print(object.key)
         sizeB = sizeB + object.size
         TotalGB = sizeB / 1000 / 1024 / 1024
     print(sizeB, "Bytes")
-    print("Total Size: ", TotalGB)
+    print("==========\nTotal Size: ", TotalGB)
+
+def s3_list_folder_objects(folder):
+    print("Listing object keys under", folder)
+    print("-------------------------------------------")
+    for bucket in s3.buckets.all():
+        for obj in bucket.objects.filter(Prefix = folder):
+            print('{0}:{1}'.format(bucket.name, obj.key))
 
 if __name__ == '__main__':
+    s3 = boto3.resource('s3')
     #List Buckets
-    print("\nBucket List")
+    print("\nBucket List\n-----------\n")
     print(s3_list())
     #List Size of objects in a bucket and it's total size
-    print("\nObjects/Sizes and Total Bucket Size")
+    print("\nObjects/Sizes and Total Bucket Size\n-----------------------------------\n")
     s3_bucket_size('elasticbeanstalk-ap-southeast-2-539615378239')
+    print('\n')
+    s3_list_folder_objects('folder1/')
